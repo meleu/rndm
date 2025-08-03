54a1b81001628b90b2153884a5cd0dd3fa2e0e2c
meleu <meleu.dev@gmail.com>
2025-08-03 16:29:17 -0300

    Actual logic to print a random number
    Note that the contents of the `root_command.sh` file is wrapped by a
    function called `root_command()`.

--- src/root_command.sh
+++ src/root_command.sh
@@ -1,5 +1,1 @@
-echo "# This file is located at 'src/root_command.sh'."
-echo "# It contains the implementation for the 'rndm' command."
-echo "# The code you write here will be wrapped by a function named 'rndm_command()'."
-echo "# Feel free to edit this file; your changes will persist when regenerating."
-inspect_args
+echo "$RANDOM"
