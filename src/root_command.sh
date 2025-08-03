bd4cfd0ef637570dc2e3649b9be7b28d1a4fdda7
meleu <meleu.dev@gmail.com>
2025-08-03 16:44:32 -0300

    Add the '--web' flag
    Some people take randomness very seriously (especially those who deal
    with cryptography). Well, there's a web service called random.org that
    is self described as "a true random number service that generates
    randomness via atmospheric noise". The https://random.org website exists
    since 1998 and is still running, then it must be true that they're good
    with randomness.
    
    They offer an HTTP API endpoint where we can send a request to get
    random integers. Let's allow the users of `rndm` choose if they want
    their random numbers from the `$RANDOM` variable or from random.org
    
    First, let's add the `flag:` directive in our `bashly.yml`. Look at the
    added lines and then run `./rndm --help` to see how such a simple
    addition to the yaml generated a nice looking help message.
    
    Then look at the implementation: a simple `curl` call sending a GET
    request to the random.org endpoint responsible to generate integers.
    
    The endpont was obtained from https://www.random.org/clients/http/api/

--- src/root_command.sh
+++ src/root_command.sh
@@ -1,1 +1,8 @@
-echo "$RANDOM"
+if [[ "${args[--web]}" == 1 ]]; then
+  curl \
+    --silent \
+    --location \
+    "https://www.random.org/integers/?num=1&min=0&max=32767&col=1&base=10&format=plain"
+else
+  echo "$RANDOM"
+fi
