2ae621fa596850c4cb4ac22a5b72305936589804
meleu <meleu.dev@gmail.com>
2025-08-04 08:15:34 -0300

    Validate --max argument as a positive integer
    Here we're using a regular expression to validate the input as positive
    integer. This makes our program stronger but our code is starting to
    become unpleasant to read.
    
    In the next commit we're going to start using functions with clear name
    to make our code easier to read and maintain.

--- src/root_command.sh
+++ src/root_command.sh
@@ -1,5 +1,10 @@
 max_number="${args[--max]}"
 
+if ! [[ "$max_number" =~ ^[1-9][0-9]*$ ]]; then
+  echo "The argument must be a positive integer. Given value: $max_number"
+  exit 1
+fi
+
 if [[ "${args[--web]}" == 1 ]]; then
   curl \
     --silent \
