c42f2a09a51d0294a448ed6d11ae53e4d347a3d1
meleu <meleu.dev@gmail.com>
2025-08-04 17:53:38 -0300

    Add '--min' flag to specify minimum random number
    We are adding the possibility to specify the minimum random number to be
    generated.
    
    Here we're also introducing short options: `-M` for `--max`, and `-m`
    for `--min`.
    
    But, what happens if the user gives a `--min` that is greater than
    `--max`? Let's handle it in the next commit...

--- src/root_command.sh
+++ src/root_command.sh
@@ -1,10 +1,12 @@
 max_number="${args[--max]}"
+min_number="${args[--min]}"
 
 if [[ "${args[--web]}" == 1 ]]; then
   curl \
     --silent \
     --location \
-    "https://www.random.org/integers/?num=1&min=0&max=${max_number}&col=1&base=10&format=plain"
+    "https://www.random.org/integers/?num=1&min=${min_number}&max=${max_number}&col=1&base=10&format=plain"
 else
-  echo $((RANDOM % max_number + 1))
+  range=$((max_number - min_number + 1))
+  echo $((RANDOM % range + min_number))
 fi
